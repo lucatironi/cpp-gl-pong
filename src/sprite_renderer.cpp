edd30f12cb08a6dd1c0eb9e4acef91b2e4a0517c
Luca Tironi <luca.tironi@gmail.com>
2018-03-19 22:33:24 +0100

    Remove textured sprite support

--- src/sprite_renderer.cpp
+++ src/sprite_renderer.cpp
@@ -1,5 +1,7 @@
 #include "sprite_renderer.hpp"
 
+#include <iostream>
+
 SpriteRenderer::SpriteRenderer(Shader &shader)
 {
     this->shader = shader;
@@ -11,26 +13,21 @@ SpriteRenderer::~SpriteRenderer()
     glDeleteVertexArrays(1, &this->quadVAO);
 }
 
-void SpriteRenderer::DrawSprite(Texture2D &texture, glm::vec2 position, glm::vec2 size, GLfloat rotate, glm::vec3 color)
+void SpriteRenderer::DrawSprite(glm::vec2 position, glm::vec2 size, GLfloat rotate, glm::vec3 color)
 {
     // Prepare transformations
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(position, 0.0f));
+
+    model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));
+    model = glm::rotate(model, rotate, glm::vec3(0.0f, 0.0f, 1.0f));
+    model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));
+
+    model = glm::scale(model, glm::vec3(size, 1.0f));
+
     this->shader.Use();
-    glm::mat4 model;
-    model = glm::translate(model, glm::vec3(position, 0.0f)); // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
-
-    model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));   // Move origin of rotation to center of quad
-    model = glm::rotate(model, rotate, glm::vec3(0.0f, 0.0f, 1.0f));                // Then rotate
-    model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f)); // Move origin back
-
-    model = glm::scale(model, glm::vec3(size, 1.0f)); // Last scale
-
     this->shader.SetMatrix4("model", model);
-
-    // Render textured quad
     this->shader.SetVector3f("spriteColor", color);
-
-    glActiveTexture(GL_TEXTURE0);
-    texture.Bind();
 
     glBindVertexArray(this->quadVAO);
     glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -42,14 +39,14 @@ void SpriteRenderer::initRenderData()
     // Configure VAO/VBO
     GLuint VBO;
     GLfloat vertices[] = {
-        // Pos      // Tex
-        0.0f, 1.0f, 0.0f, 1.0f,
-        1.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 0.0f, 0.0f,
+        // Pos
+        0.0f, 1.0f,
+        1.0f, 0.0f,
+        0.0f, 0.0f,
 
-        0.0f, 1.0f, 0.0f, 1.0f,
-        1.0f, 1.0f, 1.0f, 1.0f,
-        1.0f, 0.0f, 1.0f, 0.0f};
+        0.0f, 1.0f,
+        1.0f, 1.0f,
+        1.0f, 0.0f};
 
     glGenVertexArrays(1, &this->quadVAO);
     glGenBuffers(1, &VBO);
@@ -59,7 +56,7 @@ void SpriteRenderer::initRenderData()
 
     glBindVertexArray(this->quadVAO);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid *)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid *)0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
