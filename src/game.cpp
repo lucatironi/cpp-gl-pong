7e2ed3428be1f5de9b1f2bd2eb21935031042ef3
Luca Tironi <luca.tironi@gmail.com>
2018-03-19 22:31:41 +0100

    Remove dummy test sprite

--- src/game.cpp
+++ src/game.cpp
@@ -22,8 +22,6 @@ void Game::Init()
     glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width), static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
     ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
     ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
-    // Load textures
-    ResourceManager::LoadTexture("/Users/puddu/code/cpp/games/pong/assets/awesomeface.png", GL_TRUE, "face");
     // Set render-specific controls
     Shader myShader;
     myShader = ResourceManager::GetShader("sprite");
@@ -40,7 +38,4 @@ void Game::ProcessInput(GLfloat dt)
 
 void Game::Render()
 {
-    Texture2D myTexture;
-    myTexture = ResourceManager::GetTexture("face");
-    Renderer->DrawSprite(myTexture, glm::vec2(200, 200), glm::vec2(300, 400), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
